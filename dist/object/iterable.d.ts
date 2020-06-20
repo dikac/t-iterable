a4d750e3df5b1c1b7343c89f87a8725a67a3c418
dikac <rigel234@yahoo.com>
2020-06-20 09:18:26 +0700

    remove to "object"

--- dist/object/iterable.d.ts
+++
@@ -1,4 +0,0 @@
-/**
- * Convert iterable to object
- */
-export default function Iterable<Property extends string | symbol | number, Value extends any>(iterable: globalThis.Iterable<[Property, Value]>): Record<Property, Value>;
